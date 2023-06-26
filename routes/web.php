7ae6dc542eb382fb106cae51e289ee2b2005eda7
Cristian Hernandez <crissstianfhz@gmail.com>
2023-06-26 17:26:46 -0500

    Se agrego el envio de emails

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\EmailController;
 
 Route::get('/', [EventController::class, 'index'])->name('index');
 Route::get('/comprar/{id}', [PaymentController::class, 'comprar']);
 Route::post('/pago', [PaymentController::class, 'pago']);
 Route::get('eventos/{id}', [EventController::class, 'show']);
+Route::get('/send-email', [EmailController::class, 'index']);
 
 Route::middleware([
     'auth:sanctum',
